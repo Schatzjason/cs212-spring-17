5c75f68b2d085aa5153ecc5d0175ad8c155ff3ca
Jason Schatz <jason.schatz@udacity.com>
2017-04-06 18:01:39 -0700

    add movielist1

---
+++ Networking/MovieList1/MovieList/MovieListViewController.swift
@@ -0,0 +1,148 @@
+//
+//  MovieListViewController.swift
+//  MovieList
+//
+//  Created by ccsfcomputers on 10/29/15.
+//  Copyright (c) 2015 Jason Schatz. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var movies: [Movie] = [Movie]()
+    
+    override func viewDidLoad() {
+        
+        setUIToDownloading(true)
+        
+        let url = TMDBURLs.popular()
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+
+            // Simple error handling
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            // Update the view controller's state
+            self.movies = self.moviesFromData(data)
+            
+            // Send the UI Updating work back to the main thread
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.setUIToDownloading(false)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: - Toggle UI while downloading
+    
+    func setUIToDownloading(_ isDownloading: Bool) {
+        
+        if isDownloading {
+            self.activityIndicator.startAnimating()
+        } else {
+            self.activityIndicator.stopAnimating()
+        }
+        
+        self.activityIndicator.isHidden = isDownloading
+        self.tableView.alpha = isDownloading ? 0.2 : 1.0
+    }
+    
+    // MARK: - Table View
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    var cellNumber = 0
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        // Get the movie associated with this row out of the array
+        var movie = movies[indexPath.row]
+        
+        // Set the movie title
+        cell.textLabel!.text = movie.title
+        
+        // Set the movie poster
+        
+        if movie.posterPath == nil {
+            // api node has no imagepath
+            cell.imageView!.image = UIImage(named: "noImage")
+        } else {
+            // Set a placeholder before we start the download
+            cell.imageView!.image = UIImage(named: "placeHolder")
+        
+            // get url, 
+            let url = TMDBURLs.URLForPosterWithPath(movie.posterPath!)
+
+            // create task
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {
+                data, response, error in
+
+                if let error = error {
+                    print(error)
+                }
+
+                if let data = data, let image = UIImage(data: data) {
+                    movie.posterImage = image
+                    DispatchQueue.main.async {
+                        cell.imageView!.image = image
+                    }
+                }
+            }) 
+            
+            // resume task
+            task.resume()
+            
+        }
+        
+        return cell
+    }
+    
+    
+    // MARK: - Parser
+    
+    func moviesFromData(_ data: Data?) -> [Movie] {
+        
+        // No data, return an empty array
+        guard let data = data else {
+            return []
+        }
+        
+        // Parse the Data into a JSON Object
+        let JSONObject = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0))
+        
+        // Insist that this object must be a dictionary
+        guard let JSONDictionary = JSONObject as? [String : AnyObject] else {
+            assertionFailure("Failed to parse data. data.length: \(data.count)")
+            return [Movie]()
+        }
+        
+        // Print the object, for now, so we can take a look
+        //print(JSONDictionary)
+        
+        // Pretty Print the string, for debugging
+        let prettyData = try! JSONSerialization.data(withJSONObject: JSONObject, options: .prettyPrinted)
+        let prettyString = String(data: prettyData, encoding: String.Encoding.utf8)
+        print(prettyString ?? "No String Available")
+        
+        let movieDictionaries = JSONDictionary[TMDB.Keys.Results] as! [[String : AnyObject]]
+        
+        let movies = movieDictionaries.map() {
+            Movie(dictionary: $0)!
+        }
+        
+        return movies
+    }
+}
---
+++ Networking/MovieList1/MovieList/TheMovieDB/TMDBURLs.swift
@@ -0,0 +1,105 @@
+//
+//  TheMovieDB.swift
+//  TheMovieDB
+//
+//  Created by Jason Schatz on 1/10/15.
+//
+
+import Foundation
+
+struct TMDBURLs {
+
+    // MARK: - URL Helper
+    
+    static let IDPlaceholder: String = "{id}"
+    
+    static func popular() -> URL {
+        return URLForResource(resource: TMDB.Resources.MoviePopular)
+    }
+        
+    static func URLForResource(resource r: String, parameters p: [String : AnyObject] = [:]) -> URL {
+        
+        // make mutable copies of the resource and parameters
+        var resource = r
+        var parameters = p
+        
+        // Add in the API Key
+        parameters["api_key"] = TMDB.Constants.ApiKey as AnyObject?
+        
+        // Substitute the id parameter in place of the IDPlaceholder
+        if resource.range(of: IDPlaceholder) != nil {
+            
+            // get the id string from the parameters
+            let id = "\(parameters[TMDB.Keys.ID]!)"
+            
+            // rewrite the resource with the {id} replaced with the parameter id
+            resource = resource.replacingOccurrences(of: IDPlaceholder, with: id)
+            
+            // remove the id from the parameters
+            parameters.removeValue(forKey: TMDB.Keys.ID)
+        }
+        
+        // turn the remaining parameters into a string
+        let paramString = parameterString(parameters)
+        
+        // Assemble the URL String
+        let urlString = TMDB.Constants.BaseURL + resource + "?" + paramString
+        
+        // Create a URL
+        let url = URL(string: urlString)!
+        
+        return url
+    }
+    
+    // The methods for poster image URL's is almost exactly like the method for people's profile image
+    // URL's.  The only diference is the value for the defalut value of the "size" parameter.
+    //
+    // TMDB has different constants for images sizes. You can find the arrays of size values in the TMDB struct
+    static func URLForPosterWithPath(_ filePath: String, size: String? = TMDB.Images.PosterSizes[1]) -> URL {
+        
+        let baseURL = URL(string: TMDB.Constants.BaseImageURL)!
+        
+        return baseURL.appendingPathComponent(size!).appendingPathComponent(filePath)
+    }
+    
+    static func URLForProfileWithPath(_ filePath: String, size: String? = TMDB.Images.ProfileSizes[1]) -> URL {
+        
+        let baseURL = URL(string: TMDB.Constants.BaseImageURL)!
+        
+        return baseURL.appendingPathComponent(size!).appendingPathComponent(filePath)
+    }
+    
+    
+    // Translate a dictionary of key/values into a URL encoded parameter string
+    
+    fileprivate static func parameterString(_ parameters: [String : AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        }
+        
+        // And array of strings. Each element will have the format "key=value"
+        var urlKeyValuePairs = [String]()
+
+        for (key, value) in parameters {
+            
+            // make sure that it is a string value
+            let stringValue = "\(value)"
+            
+            // Escape it
+            let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+            
+            // Append it
+            
+            if let escapedValue = escapedValue {
+                let keyValuePair = "\(key)=\(escapedValue)"
+                urlKeyValuePairs.append(keyValuePair)
+            } else {
+                print("Warning: trouble escaping string \"\(stringValue)\"")
+            }
+        }
+        
+        // Create a single string, separated with &'s
+        return urlKeyValuePairs.joined(separator: "&")
+    }
+}
